1d4f782bb0406984e0731c3dd0434e869f23e4b7
novenebulae <mail.lucas.olivarez@gmail.com>
2025-04-16 09:39:15 +0200

    Created structure creation page

---
+++ src/app/pages/private/staff/panels/structure-creation/structure-creation/structure-creation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-structure-creation',
+  imports: [],
+  templateUrl: './structure-creation.component.html',
+  styleUrl: './structure-creation.component.scss'
+})
+export class StructureCreationComponent {
+
+}
